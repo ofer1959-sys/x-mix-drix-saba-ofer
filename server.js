f8413191e72f0139208d997c85f91b9ba92e0bbb
ofer1959-sys <ofer1959@gmail.com>
2026-04-21 23:57:09 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static('public'));
+// הנה השורה שתוקנה - עכשיו השרת יודע בדיוק איפה התיקייה
+app.use(express.static(path.join(__dirname, 'public')));
 
 const rooms = {};
 
